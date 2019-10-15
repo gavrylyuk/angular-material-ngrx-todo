1fa8d103957a662772a3ed63ac82483ec90e8129
Grygoriy Gavrylyuk <ggavrylyuk@gmail.com>
2019-10-15 10:34:50 +0300

    [dev] added interface, service, mock
    Added ToDo interface, mock data example, basic service with methods.

---
+++ src/app/service/task.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { TASKS } from './tasks-mock';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class TaskService {
+
+
+  constructor() { }
+
+  getTasks() {
+    return TASKS;
+  }
+
+  addTask(
+    id: number,
+    title: string,
+    description: string,
+    comolete: boolean
+  ) {
+    return TASKS.push({
+      id,
+      title,
+      description,
+      complete: false
+    });
+  }
+
+  deleteTask() {}
+
+  editTask() {}
+
+  complete() {}
+}
---
+++ src/app/service/tasks-mock.ts
@@ -0,0 +1,7 @@
+import {ITask} from './task.interface';
+
+export const TASKS: ITask[] = [
+    {id: 1, title: 'First task', description: 'description 333', complete: false},
+    {id: 2, title: 'Second task', complete: false},
+    {id: 3, title: 'Third task', description: '', complete: true},
+];
